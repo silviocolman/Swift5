e587239eff619306f968d6b557bc2fc1f88678fe
Silvio Colman <silviorafael.colman@gmail.com>
2020-06-23 19:01:05 -0400

    Variables

---
+++ 001-Variables.playground/Contents.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+//Variales: valores que van a cambiar
+//Constantes: valores de codígo que nunca cambiarán
+let maximumNumberOfLogins = 10
+
+var currentLoginAttempt = 0
+
+var x = 0.0, y = 0.0, z = 0.0
+
+var welcomeMessage : String
+
+welcomeMessage = "Hola"
+
+var red, green, blue: Double
+
+let π = 3.14159265
+
+print(welcomeMessage)
+
+print([1,2,3,4,5], separator: "-", terminator: ".")
+
+//let muerte = "muerte"
+
+/*  Esto es un comentario
+    multilinea
+*/
+
+
+
+ 
+
+
+
